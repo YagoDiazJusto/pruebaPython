21a1db9fb2a2936bc360665aec5a38d3e42e4509
Yago <a22yagodj@iessanclemente.net>
2025-03-24 18:39:48 +0100

    archivos

---
+++ utils.py
@@ -0,0 +1,26 @@
+import math
+
+def is_prime(n):
+    if n < 0:
+        return "Os números negativos non están permitidos"
+    if n <= 1:
+        return False
+    if n == 2:
+         return True
+    if n % 2 == 0:
+        return False
+    for i in range(2, int(math.sqrt(n)), 1):
+        if n % i == 0:
+            return False
+    return True
+
+def multiplicar(n,m):
+    return n*m
+
+def dividir(n,m):
+    return n/m
+
+def cubic(a):
+    return a * a * a
+def say_hello(nome):
+    return "Ola, " + nome
