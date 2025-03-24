84173c411643b1495bc683227860d06d90662225
Yago Diaz Justo <114486721+YagoDiazJusto@users.noreply.github.com>
2025-03-24 19:41:50 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,21 +6,24 @@ def is_prime(n):
     if n <= 1:
         return False
     if n == 2:
-         return True
+        return True
     if n % 2 == 0:
         return False
-    for i in range(2, int(math.sqrt(n)), 1):
+    for i in range(2, int(math.sqrt(n)) + 1):
         if n % i == 0:
             return False
     return True
 
-def multiplicar(n,m):
-    return n*m
+def multiplicar(n, m):
+    return n * m
 
-def dividir(n,m):
-    return n/m
+def dividir(n, m):
+    if m == 0:
+        raise ValueError("Non se pode dividir por cero")
+    return n / m
 
 def cubic(a):
     return a * a * a
+
 def say_hello(nome):
     return "Ola, " + nome
